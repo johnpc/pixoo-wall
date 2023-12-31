cc80e0dc516677536617547b44e0a9401a617b65
John Corser <johnpc@umich.edu>
2023-12-31 11:48:24 -0500

    allow long messages to marquee across board

--- scripts/update-wall.ts
+++ scripts/update-wall.ts
@@ -3,16 +3,40 @@ import dotenv from 'dotenv';
 import { PixooAPI, Color } from 'pixoo-api';
 dotenv.config();
 
-const main = async () => {
-	const mostRecentMessage = await getCurrentMessage();
+const MAX_FRAME_CHARS = 40;
+const sleep = async () => await new Promise(resolve => setTimeout(resolve, 500));
+const getTextFrame = (message: string, frame: number) => {
+  if (message.length < MAX_FRAME_CHARS) {
+    return message;
+  }
 
+  return message.substring(frame, MAX_FRAME_CHARS + frame);
+}
+
+const main = async () => {
 	console.log({ip: process.env.PIXOO_IP!})
 	const pixoo = new PixooAPI(process.env.PIXOO_IP!);
 	await pixoo.initialize();
 	console.log({initialized: true});
-	pixoo.drawText(mostRecentMessage?.content!, [0, 0], Color.Green)
-	console.log({drawn: true});
-	await pixoo.push()
-	console.log({pushed: true});
+
+  let frame = 0;
+  do {
+    pixoo.clear();
+    const mostRecentMessage = await getCurrentMessage();
+    const frameMessage = getTextFrame(mostRecentMessage?.content!, frame);
+    pixoo.drawText("Write a message!", [0, 0], Color.Green)
+    pixoo.drawText("--- jpc.io/wall ---", [0, 10], Color.Red)
+    pixoo.drawText("----------------", [0, 20], Color.Apricot)
+    pixoo.drawText('> ' + frameMessage, [0, 40], Color.Purple)
+
+    console.log({drawn: true});
+    await pixoo.push()
+    console.log({pushed: true});
+    frame++;
+    if (frame >= MAX_FRAME_CHARS) {
+      frame = 0;
+    }
+    await sleep();
+  } while (true);
 }
 main();
