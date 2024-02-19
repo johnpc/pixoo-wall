b22d62fade639fa67004631965a8fbe78c0fac3b
John Corser <johnpc@umich.edu>
2024-02-19 15:59:38 -0500

    add pwa manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>A2 Poll</title>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
