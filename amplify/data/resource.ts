1cfb0186747c04e3e49d5aaa5d18f26d7fc96d5d
John Corser <johnpc@umich.edu>
2024-09-06 11:46:17 -0400

    grant createJokeFunction access to data

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,10 +1,14 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { createJokeFunction } from "../function/resource";
 const schema = a.schema({
   Message: a
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.guest().to(["create", "read"])]),
+    .authorization((allow) => [
+      allow.guest().to(["create", "read"]),
+      allow.resource(createJokeFunction).to(["create", "read"]),
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
